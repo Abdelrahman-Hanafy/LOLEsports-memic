14f297e3ee00fc46314be2eb4136969e9dce9225
Abdelrahman Mohamed Mostafa <a.m.mostafa1@student.aast.edu>
2023-03-29 17:48:14 +0200

    Fixing Navbar collapse behavier

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,13 +8,12 @@ const Navbarfun = () => {
   return (
     <nav className='navbar navbar-expand-lg navbar-dark '>
         <Link to='/' className='navbar-brand'>
-            <img src={logo} width="90" height="27" className="d-inline-block align-top" alt="" />
-            {/* LOLStier */}
+            <img src={logo} width="90"  className="d-inline-block align-top" alt="" />
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav  mr-auto">
               <li className="nav-item active">
                   <Link className="nav-link" to='/schedule'>
