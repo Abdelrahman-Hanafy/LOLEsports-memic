948202df1c1e85039e0b7318fa149bb526be3b47
Abdelrahman Mohamed Mostafa <a.m.mostafa1@student.aast.edu>
2023-03-29 23:37:39 +0200

    Adding rewards page

--- src/Components/Rewards.jsx
+++ src/Components/Rewards.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './rewards.css'
+import rewards from '../assets/rewards.png'
 
 const Rewards = () => {
   return (
     <div className='content  with-out'>
-      <h1>Rewards</h1>
+      <div className="RewardsStartEarning">
+        <div className="text">
+          <div className="title">Watch Games. Earn Drops and Rewards.</div>
+          <div className="body">Earn exclusive icons, emotes, and other loot. To get started, we need to track your watch patterns to give you rewards and drops. View our privacy policy. </div>
+        </div>
+        <div className="image">
+          <img src={rewards} alt=''/>
+        </div>
+      </div>
     </div>
     
   )
